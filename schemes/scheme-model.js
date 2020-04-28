a15e72446d0b24f7652f3a19e247f9d27dbded6e
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-04-28 03:08:27 -0400

    regular commits

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,14 +3,23 @@
 const db = require("../data/dbConfig");
 
     function find(){
-        return db.select().from("schemes");
+        return db("schemes");
 }
 
     function findById(id){
-
+        if (!id) {
+            return null;
+        }
+        return db("schemes").where({ id }).first();
 }
 
-    function findSteps(){
+function findSteps(id) {
+    return db.select("steps.id as step_id ", "schemes.scheme_name",
+        "steps.step_number", "steps.instructions")
+    .from("schemes")
+    .join("steps", "schemes.id", "steps.scheme_id");
+    where({ id });
+       
 
 }
 
